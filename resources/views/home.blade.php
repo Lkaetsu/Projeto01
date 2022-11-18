f46bd396ac1d3574fb5a4026b2ff165eb7c4da9c
Lkaetsu <leonardo.kaetsu9@gmail.com>
2022-11-18 19:25:59 -0300

    ProjetoV0.1(setup)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="card text-white" style="background-color:rgb(0, 0, 255);">
+        <div class="card-body">
+            <h1 class="card-title">$Nome_Escola$</h1>
+            <p class="card-text">Disponibiliza cursos online</p>
+        </div>
+    </div>
+    <h2><span class="badge bg-primary">Cursos</span></h2>
+    <div class="row row-cols-1 row-cols-2">
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Curso1</h3>
+                    <p class="card-text">Nesse curso o aluno aprederá a...</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Curso2</h3>
+                    <p class="card-text">Nesse curso o aluno aprederá a...</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Curso3</h3>
+                    <p class="card-text">Nesse curso o aluno aprederá a...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/Area-Admin', function () {
+    return view('A_adm');
+});
+
+Route::get('/Area-Secretaria', function () {
+    return view('A_sec');
+});
+
+Route::get('/Area-Professor', function () {
+    return view('A_prof');
+});
+
+Route::get('/Area-Aluno', function () {
+    return view('A_alu');
 });
